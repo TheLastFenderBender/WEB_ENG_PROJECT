da7e0344bb5c0c5b9baec70a2bd0295cf4f62854
Umar.exe <61727014+yuri8822@users.noreply.github.com>
2023-12-15 22:04:40 +0500

    started work on my landing page

---
+++ Frontend/src/FlightManagerPage.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+// this page will be the landing page for the flight manager.
+// it will display all the flights in the database in a tile format.
+// each tile will have a button to view the flight details
+// and a button to edit the flight details.
+// there will also be a button to add a new flight.
+// the page will also have a search bar to search for flights by flight number.
+// the search bar will be a separate component.
+// the flight tiles will be a separate component.
+// the flight details will be a separate component.
+// the flight edit will be a separate component.
+// the flight add will be a separate component.
+
+const FlightManagerPage = () => {
+
+    const response = fetch('http://localhost:3000/flights');
+
+            
+
+    return (
+        <div>
+            <h1>Flight Manager Page</h1>
+            
+        </div>
+    );
+}
+
+export default FlightManagerPage;
